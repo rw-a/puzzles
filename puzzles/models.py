c3d81c94f09e181c7f032b4f67e8b9669d9d00d0
rw-a <rwa2684@gmail.com>
2024-02-28 18:35:44 +1000

    feat: create models

--- puzzles/models.py
+++ puzzles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Puzzle(models.Model):
+    name = models.CharField(max_length=100)
+    identifier = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField()
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+
+
+class Hint(models.Model):
+    description = models.TextField()
+    order = models.IntegerField()
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
