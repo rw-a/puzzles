cca48d2870afed87986b2c56edab5477ef26396b
rw-a <rwa2684@gmail.com>
2024-02-28 18:07:08 +1000

    change: update locale

--- OrienteeringPuzzles/settings.py
+++ OrienteeringPuzzles/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-AU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Queensland'
 
 USE_I18N = True
 
