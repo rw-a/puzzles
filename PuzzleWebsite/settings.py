649003c5de1d0ad7c11f4822f89055d11d9e205b
rw-a <rwa2684@gmail.com>
2024-02-29 17:35:19 +1000

    feat: load settings from env variables

--- PuzzleWebsite/settings.py
+++ PuzzleWebsite/settings.py
@@ -1,19 +1,17 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = bool(os.getenv('DEBUG'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@%9nm$5t#i0w901y0p8n4l5og%kh=jwqjlq-aw(zb@stk@j!%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 
 # Application definition
